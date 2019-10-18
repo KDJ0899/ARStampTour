ec4ad4406fdb5577c1344661fc5eef0a2683b87e
HG <vegavenus0920@gmail.com>
2019-10-18 14:05:48 +0900

    map이동

--- app/src/main/java/com/example/arstest/mapPage.java
+++ app/src/main/java/com/example/arstest/mapPage.java
@@ -8,22 +8,14 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -34,27 +26,18 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class mapPage extends AppCompatActivity implements OnMapReadyCallback {
 
-    private Button button1, addBtn, back;
+    private Button back;
     private Button homeBtn,rewardBtn,userBtn,mypageBtn,searchBtn;
     LinearLayout layout;
-    private TextView txtResult, landmarkInformation;
-    private RecyclerView recyclerView;
-    public LinearLayoutManager layoutManager;
-    private RecyclerView.Adapter adapter;
-    ArrayAdapter<CharSequence> adspin1, adspin2;
-    String selectedCity="";
-    String selectedCityDetail="";
     Context context = this;
-    int btnCount=0;
     private GoogleMap mMap;
     double longitude, latitude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map_page);
-        button1 = (Button)findViewById(R.id.button1);
-        txtResult = (TextView)findViewById(R.id.txtResult);
+        setContentView(R.layout.activity_search);
         back = (Button)findViewById(R.id.backBtn);
         homeBtn = findViewById(R.id.imageHome);
         rewardBtn = findViewById(R.id.imageReward);
@@ -62,13 +45,13 @@ public class mapPage extends AppCompatActivity implements OnMapReadyCallback {
         mypageBtn = findViewById(R.id.profile);
         searchBtn = findViewById(R.id.imageSearch);
 
-        back.setOnClickListener(new View.OnClickListener() {
+       /* back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),homePage.class);
                 startActivity(intent);
             }
-        });
+        });*/
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -105,120 +88,8 @@ public class mapPage extends AppCompatActivity implements OnMapReadyCallback {
             }
         });
 
-        landmarkInformation = (TextView)findViewById(R.id.landmarkInformation);
-        landmarkInformation.setText("지역을 선택하세요.");
-
-        recyclerView = findViewById(R.id.recyclerView);
-
-        layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-        recyclerView.setLayoutManager(layoutManager);
-
-        adapter = new RecyclerViewA();
-
-        /*LinearLayout layout = (LinearLayout)findViewById(R.id.linearlayout);
-        Button btn = new Button(this);
-        btn.setText("투어 참여하기");
-        layout.addView(btn);*/
-
-        final Spinner spin1 = (Spinner)findViewById(R.id.spinner);
-        final Spinner spin2 = (Spinner)findViewById(R.id.spinner2);
-        Button btn_search = (Button)findViewById(R.id.btn_search);
-
-        adspin1 = ArrayAdapter.createFromResource(this, R.array.city, android.R.layout.simple_spinner_dropdown_item);
-
-        adspin1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spin1.setAdapter(adspin1);
-
-        spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
-                if (adspin1.getItem(i).equals("서울특별시")) {
-
-                    selectedCity = "서울특별시"; //출력을 위한 값 입력
-                    adspin2 = ArrayAdapter.createFromResource(mapPage.this, R.array.city_seoul, android.R.layout.simple_spinner_dropdown_item);
-
-
-
-                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    spin2.setAdapter(adspin2);
-                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                        //두번째 spinner 선택 이벤트를 정의합니다.
-                        @Override
-                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                            selectedCityDetail = adspin2.getItem(i).toString();
-
-                        }
-
-                        @Override
-                        public void onNothingSelected(AdapterView<?> adapterView) {
-
-                        }
-                    });
-                } else if (adspin1.getItem(i).equals("부산광역시")) {
-
-                    selectedCity = "부산광역시";
-                    adspin2 = ArrayAdapter.createFromResource(mapPage.this, R.array.city_busan, android.R.layout.simple_spinner_dropdown_item);
-                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                    spin2.setAdapter(adspin2);
-                    spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                        @Override
-                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                            selectedCityDetail = adspin2.getItem(i).toString();
-                        }
-
-                        @Override
-                        public void onNothingSelected(AdapterView<?> adapterView) {
-                        }
-                    });
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-        btn_search.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View view) {
-
-                Toast.makeText(mapPage.this, "선택된 도시는 " + selectedCity + " " + selectedCityDetail+" 입니다.", Toast.LENGTH_SHORT).show();
-                landmarkInformation.setText("선택 지역 " + selectedCity + " "+  selectedCityDetail +" 의 "+"획득 가능 스탬프 수는 15개 입니다.");
-                recyclerView.setAdapter(adapter);
-                Button btn = new Button(context);
-                btn.setText("투어 신청하기");
-              //  btn.setGravity(Gravity.CENTER_VERTICAL);
-                layout = (LinearLayout)findViewById(R.id.linearlayout);
-
-                LinearLayout.LayoutParams param = new LinearLayout.LayoutParams
-                        (LinearLayout.LayoutParams.MATCH_PARENT,
-                                LinearLayout.LayoutParams.MATCH_PARENT);
-                layout.setLayoutParams(param);
-
-                ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams
-                        (ViewGroup.LayoutParams.WRAP_CONTENT,
-                                ViewGroup.LayoutParams.WRAP_CONTENT);
-                btn.setLayoutParams(lp);
-                if(btnCount==0){
-
-                    layout.addView(btn);}
-                btnCount++;
-            }
-        });
-
-
         final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 if ( Build.VERSION.SDK_INT >= 23 &&
                         ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
                     ActivityCompat.requestPermissions( mapPage.this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
@@ -231,10 +102,6 @@ public class mapPage extends AppCompatActivity implements OnMapReadyCallback {
                     latitude = location.getLatitude();
                     double altitude = location.getAltitude();
 
-                    txtResult.setText("위치정보 : " + provider + "\n" +
-                            "위도 : " + longitude + "\n" +
-                            "경도 : " + latitude + "\n" +
-                            "고도  : " + altitude);
 
                     lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                             1000,
@@ -254,23 +121,10 @@ public class mapPage extends AppCompatActivity implements OnMapReadyCallback {
             }
 
 
-        });
-
-
-
-    }
     final LocationListener gpsLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
-
-            String provider = location.getProvider();
             longitude = location.getLongitude();
             latitude = location.getLatitude();
-            double altitude = location.getAltitude();
-
-            txtResult.setText("위치정보 : " + provider + "\n" +
-                    "위도 : " + longitude + "\n" +
-                    "경도 : " + latitude + "\n" +
-                    "고도  : " + altitude);
 
         }
 
@@ -301,6 +155,5 @@ public class mapPage extends AppCompatActivity implements OnMapReadyCallback {
         mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
 
+
 }
-
-
--- app/src/main/java/com/example/arstest/rewardPage.java
+++ app/src/main/java/com/example/arstest/rewardPage.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +21,10 @@ import java.util.ArrayList;
 public class rewardPage extends AppCompatActivity implements View.OnClickListener
 {
     private ListView m_oListView = null;
-    private Button homeBtn,searchBtn,userBtn,mapBtn,mypageBtn;
+    private Button homeBtn,searchBtn,userBtn,mapBtn,mypageBtn, btn_search;
+    ArrayAdapter<CharSequence> adspin1, adspin2;
+    String selectedCategory="";
+    String selectedBrand="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,11 +37,12 @@ public class rewardPage extends AppCompatActivity implements View.OnClickListene
         userBtn = findViewById(R.id.imageMypage);
         mapBtn = findViewById(R.id.map);
         mypageBtn = findViewById(R.id.profile);
+        btn_search = findViewById(R.id.btn_search);
 
         mapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),mapPage.class);
+                Intent intent = new Intent(getApplicationContext(), searchPage.class);
                 startActivity(intent);
             }
         });
@@ -77,7 +84,7 @@ public class rewardPage extends AppCompatActivity implements View.OnClickListene
                 "2012-07-08", "1980-04-14", "2016-09-26", "2014-10-11", "2010-12-24"};
         int nDatCnt=0;
         ArrayList<ItemData> oData = new ArrayList<>();
-        for (int i=0; i<1000; ++i)
+        for (int i=0; i<30; ++i)
         {
             ItemData oItem = new ItemData();
             oItem.strTitle = "데이터 " + (i+1);
@@ -90,6 +97,79 @@ public class rewardPage extends AppCompatActivity implements View.OnClickListene
         m_oListView = (ListView)findViewById(R.id.list_cards);
         ListAdapter oAdapter = new ListAdapter(oData);
         m_oListView.setAdapter(oAdapter);
+
+
+        final Spinner category = (Spinner)findViewById(R.id.category);
+        final Spinner brand = (Spinner)findViewById(R.id.brand);
+
+        adspin1 = ArrayAdapter.createFromResource(this, R.array.category, android.R.layout.simple_spinner_dropdown_item);
+
+        adspin1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        category.setAdapter(adspin1);
+
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if (adspin1.getItem(i).equals("식품")) {
+
+                    selectedCategory = "식품"; //출력을 위한 값 입력
+                    adspin2 = ArrayAdapter.createFromResource(rewardPage.this, R.array.food_brand, android.R.layout.simple_spinner_dropdown_item);
+
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    brand.setAdapter(adspin2);
+                    brand.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        //두번째 spinner 선택 이벤트를 정의합니다.
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            selectedBrand = adspin2.getItem(i).toString();
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+
+                        }
+                    });
+                } else if (adspin1.getItem(i).equals("패션")) {
+
+                    selectedCategory = "패션";
+                    adspin2 = ArrayAdapter.createFromResource(rewardPage.this, R.array.food_brand, android.R.layout.simple_spinner_dropdown_item);
+                    adspin2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    brand.setAdapter(adspin2);
+                    brand.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            selectedBrand = adspin2.getItem(i).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(rewardPage.this, "선택된 카테고리는 " + selectedCategory + ", 선택된 브랜드는 " + selectedBrand+" 입니다.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 
 
@@ -97,13 +177,14 @@ public class rewardPage extends AppCompatActivity implements View.OnClickListene
     public void onClick(View v)
     {
         View oParentView = (View)v.getParent();
-        TextView oTextTitle = (TextView) oParentView.findViewById(R.id.textTitle);
+        TextView rewardTitle = (TextView) oParentView.findViewById(R.id.rewardTitle);
+        TextView stampAmt = (TextView) oParentView.findViewById(R.id.stampAmt);
         String position = (String) oParentView.getTag();
 
         AlertDialog.Builder oDialog = new AlertDialog.Builder(this,
                 android.R.style.Theme_DeviceDefault_Light_Dialog);
 
-        oDialog.setMessage(oTextTitle.getText()+" 상품을 구매하시겠습니까?")
+        oDialog.setMessage(rewardTitle.getText()+" 상품을 구매하시겠습니까?")
                 .setPositiveButton("예", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -118,4 +199,6 @@ public class rewardPage extends AppCompatActivity implements View.OnClickListene
             }
         }).setCancelable(false).show();
     }
+
+
 }
