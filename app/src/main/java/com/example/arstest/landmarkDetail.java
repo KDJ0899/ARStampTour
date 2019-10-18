e6127f460e949becd36500dfe76ef7bb24865669
tsb2030 <41547269+tsb2030@users.noreply.github.com>
2019-10-18 17:58:27 +0900

    landmarkdetail 서버연결

--- app/src/main/java/com/example/arstest/landmarkDetail.java
+++ app/src/main/java/com/example/arstest/landmarkDetail.java
@@ -4,32 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.arstest.server.RequestHttpURLConnection;
 import com.example.arstest.server.ServerController;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.nio.charset.Charset;
 
 public class landmarkDetail extends AppCompatActivity implements OnMapReadyCallback {
 
     public Button homeBtn,searchBtn,rewardBtn,mypageBtn,mapBtn,profileBtn;
-    static final LatLng gochuk = new LatLng(37.497774,126.866994);
+    public ImageView mainImage;
     private  GoogleMap googleMap;
+    public static JsonArray jsonArray;
+    public JsonObject jsonObject;
+    public TextView attraction,gu,si,addre,inf;
+    String attractionName,guName,siName,address,info,imageAddress;
+    LatLng latLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String attractionName = "고척스카이돔";
+        attractionName = "고척스카이돔";
         ContentValues cv = new ContentValues();
-        cv.put("from","attraction");
-        cv.put("where","name = "+attractionName);
+        cv.put("where",attractionName);
 
-        ServerController serverconn = new ServerController(cv,"getAll");
+        Log.i("--------------------------","통신전");
+        NetworkTask networkTask = new NetworkTask("http://10.0.103.96:3000/attraction", cv);
+        networkTask.execute();
+        Log.i("--------------------------","통신후");
 
         setContentView(R.layout.activity_landmark_detail);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapdetail);
@@ -92,6 +110,74 @@ public class landmarkDetail extends AppCompatActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap map) {
 
         googleMap = map;
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gochuk,16));
+        MarkerOptions makerOptions = new MarkerOptions();
+        makerOptions
+                .position(latLng)
+                .title(attractionName);
+
+        // 마커를 생성한다.
+        googleMap.addMarker(makerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if(result==null)
+                return;
+
+            JsonParser jsonParser = new JsonParser();
+            jsonArray = (JsonArray)jsonParser.parse(result);
+
+            //동적으로 변경될 것들
+            mainImage = findViewById(R.id.imageView2);
+            attraction = findViewById(R.id.attractionName);
+            gu = findViewById(R.id.guName);
+            si = findViewById(R.id.kind);
+            addre = findViewById(R.id.address);
+            inf = findViewById(R.id.textView10);
+
+            for(int i=0; i<jsonArray.size(); i++){
+                jsonObject = jsonArray.get(i).getAsJsonObject();
+                info = jsonObject.get("info").toString().replace("\"","");
+                address = jsonObject.get("Address").toString().replace("\"","");
+                imageAddress = jsonObject.get("Image").toString().replace("\"","");
+                guName = jsonObject.get("guName").toString().replace("\"","");
+                siName = jsonObject.get("siName").toString().replace("\"","");
+                String lt = jsonObject.get("Latitude").toString().replace("\"","");
+                String lg = jsonObject.get("Longitude").toString().replace("\"","");
+                latLng = new LatLng(Double.valueOf(lt).doubleValue(),Double.valueOf(lg).doubleValue());
+                Log.i("-----------받은 결과 : ","info: "+info+" address: "+address+" imageAddress: "+imageAddress+" latlng: "+latLng+" guName: "+guName+" siName: "+siName);
+                Log.i("json",jsonObject.get("Name").toString());
+            }
+
+            attraction.setText(attractionName);
+            gu.setText(guName);
+            si.setText("소속 : "+siName);
+            addre.setText(address);
+            inf.setText(info);
+            mainImage.setBackgroundResource(getApplicationContext().getResources().getIdentifier(imageAddress,"drawable",getApplicationContext().getPackageName()));
+        }
     }
 }
--- app/src/main/java/com/example/arstest/server/ServerController.java
+++ app/src/main/java/com/example/arstest/server/ServerController.java
@@ -13,24 +13,22 @@ public class ServerController  {
     private JsonObject jsonObject;
     private JsonArray jsonArray;
 
-    public ServerController(ContentValues data,String url,String method)
+    public ServerController(ContentValues data,String url)
     {
 
-        NetworkTask networkTask = new NetworkTask("http://10.0.102.44:3000"+url, data, method);
+        NetworkTask networkTask = new NetworkTask("http://192.168.43.102:3000/"+url, data);
         networkTask.execute();
     }
 
-    public class NetworkTask extends AsyncTask<Void, Void, String> {
+    public static class NetworkTask extends AsyncTask<Void, Void, String> {
 
         private String url;
         private ContentValues values;
-        private String method;
 
-        public NetworkTask(String url, ContentValues values, String method) {
+        public NetworkTask(String url, ContentValues values) {
 
             this.url = url;
             this.values = values;
-            this.method = method;
         }
 
         @Override
@@ -38,7 +36,7 @@ public class ServerController  {
 
             String result; // 요청 결과를 저장할 변수.
             RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
-            result = requestHttpURLConnection.request(url, values, method); // 해당 URL로 부터 결과물을 얻어온다.
+            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
 
             return result;
         }
@@ -50,12 +48,12 @@ public class ServerController  {
                 return;
 
             JsonParser jsonParser = new JsonParser();
-            jsonArray = (JsonArray)jsonParser.parse(result);
-
-            for(int i=0; i<jsonArray.size(); i++){
-                jsonObject = jsonArray.get(i).getAsJsonObject();
-                Log.i("json",jsonObject.get("Name").toString());
-            }
+//            jsonArray = (JsonArray)jsonParser.parse(result);
+//
+//            for(int i=0; i<jsonArray.size(); i++){
+//                jsonObject = jsonArray.get(i).getAsJsonObject();
+//                Log.i("json",jsonObject.get("Name").toString());
+//            }
 
         }
     }
